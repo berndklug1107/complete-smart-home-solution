00e27695e3cccd304714ee5081d3563f226cd86b
bernd <63996275+berndklug1107@users.noreply.github.com>
2026-01-09 17:26:08 +0100

    Refactor I2CDimmer to include I2C device support

--- components/i2c_dimmer/output.py
+++ components/i2c_dimmer/output.py
@@ -1,20 +1,21 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import output
-from esphome.const import CONF_ID, CONF_ADDRESS
+from esphome.components import output, i2c
+from esphome.const import CONF_ID
 
-i2c_dimmer_ns = cg.esphome_ns.namespace("i2c_dimmer")
-I2CDimmer = i2c_dimmer_ns.class_("I2CDimmer", output.FloatOutput, cg.Component)
+DEPENDENCIES = ['i2c']
 
-CONFIG_SCHEMA = output.FLOAT_OUTPUT_SCHEMA.extend(
-    {
-        cv.Required(CONF_ID): cv.declare_id(I2CDimmer),
-        cv.Required(CONF_ADDRESS): cv.i2c_address,  # I2C-Adresse hinzufügen
-    }
-).extend(cv.COMPONENT_SCHEMA)
 
+i2c_dimmer_ns = cg.esphome_ns.namespace('i2c_dimmer')
+
+I2CDimmer = i2c_dimmer_ns.class_('I2CDimmer', output.FloatOutput, i2c.I2CDevice, cg.Component)
+
+CONFIG_SCHEMA = output.FLOAT_OUTPUT_SCHEMA.extend({
+    cv.Required(CONF_ID): cv.declare_id(I2CDimmer),
+}).extend(cv.COMPONENT_SCHEMA).extend(i2c.i2c_device_schema(0x10))
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID], config[CONF_ADDRESS])
-    await output.register_output(var, config)
+    var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
+    await output.register_output(var, config)
+    await i2c.register_i2c_device(var, config)
